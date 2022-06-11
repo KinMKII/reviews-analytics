348554c379ea32efad94cd8eb0a90a06d37bc56e
Kin <kins2010@gmail.com>
2022-06-11 23:32:55 +0800

    calc avg lenght of reviews

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -1,13 +1,16 @@
 data =[]
 count = 0
-
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
         if count % 1000 == 0:
             print(len(data))
-print(len(data))
-print(data[0])
-print('---------------------------------------')
-print(data[1])
+print('檔案讀取完畢, 總共有', len(data), '筆資料')
+
+print('---------------------------------------------')
+
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+print('平均字數是', sum_len / len(data))
