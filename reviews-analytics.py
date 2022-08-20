dcef8f7a0230f85c4b867abad54a4abfeaf1baac
KinMKII <107061957+KinMKII@users.noreply.github.com>
2022-08-20 16:36:31 +0800

    dictionary function added

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -30,3 +30,32 @@ for d in data:
     if 'good' in d:
         good.append(d)
 print('一共有', len(good), '筆留言提到good')
+
+print('---------------------------------------------')
+
+# 文字計數
+wc = {} # word_count
+for d in data:
+    words = d.split()
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1    # 新增新的key進wc字典
+
+for word in wc:
+    if wc[word] > 10000000:
+        print(word, wc[word])   # 顥示超過10000000次出現的字
+
+print('總共有', len(wc), '個字詞加入')  # 印出字典長度
+
+while True:
+    word = input('請問你想查什麼字: ')
+    if word == 'q':
+        break
+    if word in wc:
+        print(word, '出現過的次數為: ', wc[word])
+    else:
+        print('查無此字, 請重新輸入')
+
+print('感謝使用本查詢功能')
